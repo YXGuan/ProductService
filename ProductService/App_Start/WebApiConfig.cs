bbbb37f7ea5b5b1111f8ad386dc3faf781114457
Yuxiang Guan <Yuxiang.Guan@bentley.com>
2022-01-07 11:09:44 -0500

    Add project files.

---
+++ ProductService/App_Start/WebApiConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using ProductService.Models;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+
+namespace ProductService
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // New code:
+            ODataModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Product>("Products");
+            config.MapODataServiceRoute(
+                routeName: "ODataRoute",
+                routePrefix: null,
+                model: builder.GetEdmModel());
+        }
+    }
+}
---
+++ ProductService/Controllers/ProductsController.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.AspNet.OData;
+using ProductService.Models;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+namespace ProductService.Controllers
+{
+    public class ProductsController : ODataController
+    {
+        ProductsContext db = new ProductsContext();
+        private bool ProductExists(int key)
+        {
+            return db.Products.Any(p => p.Id == key);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+
+        [EnableQuery]
+        public IQueryable<Product> Get()
+        {
+            return db.Products;
+        }
+        [EnableQuery]
+        public SingleResult<Product> Get([FromODataUri] int key)
+        {
+            IQueryable<Product> result = db.Products.Where(p => p.Id == key);
+            return SingleResult.Create(result);
+        }
+
+
+        public async Task<IHttpActionResult> Post(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            db.Products.Add(product);
+            await db.SaveChangesAsync();
+            return Created(product);
+        }
+
+
+
+        public async Task<IHttpActionResult> Patch([FromODataUri] int key, Delta<Product> product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var entity = await db.Products.FindAsync(key);
+            if (entity == null)
+            {
+                return NotFound();
+            }
+            product.Patch(entity);
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return Updated(entity);
+        }
+        public async Task<IHttpActionResult> Put([FromODataUri] int key, Product update)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (key != update.Id)
+            {
+                return BadRequest();
+            }
+            db.Entry(update).State = EntityState.Modified;
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return Updated(update);
+        }
+
+
+
+
+
+
+        public async Task<IHttpActionResult> Delete([FromODataUri] int key)
+        {
+            var product = await db.Products.FindAsync(key);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            db.Products.Remove(product);
+            await db.SaveChangesAsync();
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+
+
+    }
+
+
+}
